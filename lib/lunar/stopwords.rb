a96c65d4e0ba715a2c85db5e0c2a7f77f50096e9
Cyril David <cyx.ucron@gmail.com>
2010-06-30 05:41:31 +0800

    Make module_function declaration in stop words more explicit.

--- lib/lunar/stopwords.rb
+++ lib/lunar/stopwords.rb
@@ -5,13 +5,13 @@ module Lunar
     def include?(word)
       stopwords.include?(word.downcase)
     end
+    module_function :include?
 
   private
     def stopwords
       %w(an and are as at be but by for if in into is it no not of on or s 
          such t that the their then there these they this to was will with)
     end
-
-    module_function :stopwords, :include?
+    module_function :stopwords
   end
 end
