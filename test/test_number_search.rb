681b929a1a52a5c0fbbfeef69ff69278d0b31281
Cyril David <cyx.ucron@gmail.com>
2010-07-13 12:17:27 +0800

    Add verification tests for number searching.

--- test/test_number_search.rb
+++ test/test_number_search.rb
@@ -13,6 +13,12 @@ class NumberSearchTest < Test::Unit::TestCase
       i.number :price, 200
       i.number :category_ids, %w(100 101 102)
     end
+
+    Lunar.index Gadget do |i|
+      i.id 1002
+      i.number :price, 149
+      i.number :category_ids, %w(101 102 103)
+    end
   end
 
   test "doing straight searches for numbers" do
@@ -20,7 +26,7 @@ class NumberSearchTest < Test::Unit::TestCase
     
     assert_equal ["1001"], search.map(&:id)
   end
-
+  
   test "also doing range searches" do
     search = Lunar.search Gadget, price: 150..200
     
@@ -37,26 +43,40 @@ class NumberSearchTest < Test::Unit::TestCase
     search100 = Lunar.search Gadget, numbers: { category_ids: 100 }
     search101 = Lunar.search Gadget, numbers: { category_ids: 101 }
     search102 = Lunar.search Gadget, numbers: { category_ids: 102 }
+    search103 = Lunar.search Gadget, numbers: { category_ids: 103 }
 
     assert_equal ["1001"], search100.map(&:id)
-    assert_equal ["1001"], search101.map(&:id)
-    assert_equal ["1001"], search102.map(&:id)
+    assert_equal ["1001", "1002"], search101.map(&:id).sort
+    assert_equal ["1001", "1002"], search102.map(&:id).sort
+    assert_equal ["1002"], search103.map(&:id).sort
+  end
+
+  test "searching for a multi-id which overlaps" do
+    search100 = Lunar.search Gadget, numbers: { category_ids: 100 }
+    search101 = Lunar.search Gadget, numbers: { category_ids: 101 }
+    search102 = Lunar.search Gadget, numbers: { category_ids: 102 }
+    search103 = Lunar.search Gadget, numbers: { category_ids: 103 }
+
+    assert_equal ["1001"], search100.map(&:id)
+    assert_equal ["1001", "1002"], search101.map(&:id)
+    assert_equal ["1001", "1002"], search102.map(&:id)
+    assert_equal ["1002"], search103.map(&:id)
   end
 
   test "searching for a valid multi-id at the same time" do
     search = Lunar.search Gadget, numbers: { category_ids: %w(100 101 102) }
 
-    assert_equal ["1001"], search.map(&:id)
+    assert_equal ["1001", "1002"], search.map(&:id).sort
   end
 
   test "searching with one invalid multi-id at the same time" do
     search = Lunar.search Gadget, numbers: { category_ids: %w(100 101 102 103) }
 
-    assert_equal ["1001"], search.map(&:id)
+    assert_equal ["1001", "1002"], search.map(&:id).sort
   end
   
   test "searching with all invalid multi-id" do
-    search = Lunar.search Gadget, numbers: { category_ids: %w(103 104 105) }
+    search = Lunar.search Gadget, numbers: { category_ids: %w(104 105 106) }
     assert_equal 0, search.size
   end
 
@@ -64,6 +84,6 @@ class NumberSearchTest < Test::Unit::TestCase
     search = Lunar.search(Gadget, numbers: { category_ids: %w(100 101 102),
                                              price: [""]})
 
-    assert_equal ["1001"], search.map(&:id)
+    assert_equal ["1001", "1002"], search.map(&:id).sort
   end
 end
